95160b581969020f26e191e4bda84aa8fc89959a
craig.ian@itsligo.ie <craig.ian@itsligo.ie>
2020-12-15 14:25:43 +0000

    Initial commit

---
+++ app/src/main/java/edu/icraig/dbfrommoodledoc/MainActivity.java
@@ -0,0 +1,100 @@
+package edu.icraig.dbfrommoodledoc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DatabaseHandler db = new DatabaseHandler(this);
+
+        db.emptyHiScores();     // empty table if required
+
+        // Inserting hi scores
+        Log.i("Insert: ", "Inserting ..");
+        db.addHiScore(new HiScore("20 OCT 2020", "Frodo", 12));
+        db.addHiScore(new HiScore("28 OCT 2020", "Dobby", 16));
+        db.addHiScore(new HiScore("20 NOV 2020", "DarthV", 20));
+        db.addHiScore(new HiScore("20 NOV 2020", "Bob", 18));
+        db.addHiScore(new HiScore("22 NOV 2020", "Gemma", 22));
+        db.addHiScore(new HiScore("30 NOV 2020", "Joe", 30));
+        db.addHiScore(new HiScore("01 DEC 2020", "DarthV", 22));
+        db.addHiScore(new HiScore("02 DEC 2020", "Gandalf", 132));
+
+
+        // Reading all scores
+        Log.i("Reading: ", "Reading all scores..");
+        List<HiScore> hiScores = db.getAllHiScores();
+
+
+        for (HiScore hs : hiScores) {
+            String log =
+                    "Id: " + hs.getScore_id() +
+                            ", Date: " + hs.getGame_date() +
+                            " , Player: " + hs.getPlayer_name() +
+                            " , Score: " + hs.getScore();
+
+            // Writing HiScore to log
+            Log.i("Score: ", log);
+        }
+
+        Log.i("divider", "====================");
+
+        HiScore singleScore = db.getHiScore(5);
+        Log.i("High Score 5 is by ", singleScore.getPlayer_name() + " with a score of " +
+                singleScore.getScore());
+
+        Log.i("divider", "====================");
+
+        // Calling SQL statement
+        List<HiScore> top5HiScores = db.getTopFiveScores();
+
+        for (HiScore hs : top5HiScores) {
+            String log =
+                    "Id: " + hs.getScore_id() +
+                            ", Date: " + hs.getGame_date() +
+                            " , Player: " + hs.getPlayer_name() +
+                            " , Score: " + hs.getScore();
+
+            // Writing HiScore to log
+            Log.i("Score: ", log);
+        }
+        Log.i("divider", "====================");
+
+        HiScore hiScore = top5HiScores.get(top5HiScores.size() - 1);
+        // hiScore contains the 5th highest score
+        Log.i("fifth Highest score: ", String.valueOf(hiScore.getScore()) );
+
+        // simple test to add a hi score
+        int myCurrentScore = 40;
+        // if 5th highest score < myCurrentScore, then insert new score
+        if (hiScore.getScore() < myCurrentScore) {
+            db.addHiScore(new HiScore("08 DEC 2020", "Elrond", 40));
+        }
+
+        Log.i("divider", "====================");
+
+        // Calling SQL statement
+        top5HiScores = db.getTopFiveScores();
+
+        for (HiScore hs : top5HiScores) {
+            String log =
+                    "Id: " + hs.getScore_id() +
+                            ", Date: " + hs.getGame_date() +
+                            " , Player: " + hs.getPlayer_name() +
+                            " , Score: " + hs.getScore();
+
+            // Writing HiScore to log
+            Log.i("Score: ", log);
+        }
+
+    }
+}
