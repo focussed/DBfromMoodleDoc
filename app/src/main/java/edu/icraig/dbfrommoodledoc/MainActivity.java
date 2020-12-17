6de226002c3b7656521705c45fa2a24bb06befd7
craig.ian@itsligo.ie <craig.ian@itsligo.ie>
2020-12-17 13:58:24 +0000

    Added list view and adapter to show high scores on the screen

--- app/src/main/java/edu/icraig/dbfrommoodledoc/MainActivity.java
+++ app/src/main/java/edu/icraig/dbfrommoodledoc/MainActivity.java
@@ -4,15 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.lv);
 
         DatabaseHandler db = new DatabaseHandler(this);
 
@@ -84,17 +90,35 @@ public class MainActivity extends AppCompatActivity {
 
         // Calling SQL statement
         top5HiScores = db.getTopFiveScores();
+        List<String> scoresStr;
+        scoresStr = new ArrayList<>();
 
+        int j = 1;
         for (HiScore hs : top5HiScores) {
+
             String log =
                     "Id: " + hs.getScore_id() +
                             ", Date: " + hs.getGame_date() +
                             " , Player: " + hs.getPlayer_name() +
                             " , Score: " + hs.getScore();
 
+            // store score in string array
+            scoresStr.add(j++ + " : "  +
+                    hs.getPlayer_name() + "\t" +
+                    hs.getScore());
             // Writing HiScore to log
             Log.i("Score: ", log);
         }
+
+        Log.i("divider", "====================");
+        Log.i("divider", "Scores in list <>>");
+        for (String ss : scoresStr) {
+            Log.i("Score: ", ss);
+        }
+
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, scoresStr);
+        listView.setAdapter(itemsAdapter);
 
     }
 }
